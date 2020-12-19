8306c2408d7321ec27539e1251e1ca1683df5b90
RickyChang <guagin0972@gmail.com>
2020-12-20 00:44:54 +0800

    add timestamp as key. and sort by the key.

--- ctee_crawler.py
+++ ctee_crawler.py
@@ -3,5 +3,7 @@ import time
 class ctee_news:
     def __init__(self, title, time_string, url):
         self.title = title
-        self.time = time.strptime(time_string, "%m/%d %H:%M")
+        year = str(time.localtime().tm_year)
+        self.time = time.strptime(year+"/"+time_string, "%Y/%m/%d %H:%M")
         self.url = url
+        self.key = time.mktime(self.time)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 from ctee_crawler import ctee_news
 
-response = requests.get("https://ctee.com.tw/livenews")
+response = requests.get("https://ctee.com.tw/livenews/aj/page/1")
 
 soup = BeautifulSoup(response.text, "html.parser")
 
@@ -16,6 +16,8 @@ a = {x.contents[3] for x in result }
 b = [ x.get_text().strip().split('|') for x in a ]
 
 c = { ctee_news(x[0],x[1].strip(), '') for x in b }
+
+sorted(c, key=lambda s: s.key )
 
 for x in c:
     print(x.title, x.time.tm_year, x.time.tm_mon, x.time.tm_mday,  x.time.tm_hour, x.time.tm_min)
